7cb0e31e52b2b76f45b232780d7c25c7ecdd440d
Alex <a.matulionis@usa.com>
2019-05-22 09:03:43 -0700

    Fixed and added some things (user & compiler).

--- test.js
+++ test.js
@@ -1,21 +1,11 @@
 // compile converts Bang code (e.g. main.!) into JavaScript.
 function compile(bang) {
-  
+
   // removes comments
   bang = bang.replace(/\/\/.*?\n/g, '')
 
-  // fixed undeclared variables to have local scope
-  let declared = {}
-  let scopeReg = /{([^{]|\n)*?}/g // TODO: use to flush 'declare' each passthrough; relative per scope.
-  bang = bang.replace(/[^\s]+?\s=/g, capture => {
-    if (declared[capture]) return capture
-    declared[capture] = true
-    return `let ${capture}`
-  })
-
   // wraps needed expressions with parens
   bang = bang.replace(/(if|while|for).+?{/g, capture => {
-    // capture
     return capture
       .replace('if', 'if(')
       .replace('=', '===')
@@ -24,12 +14,71 @@ function compile(bang) {
       .replace('{', '){')
   })
 
+  // fixes function signatures
+  bang = bang.replace(/[A-Za-z\$]+\s?\([A-Za-z\$]+( [A-Za-z\$]+)*\)\s?{/g, capture => {
+    capture
+    capture = capture.replace(/\([A-Za-z\$]+( [A-Za-z\$]+)*\)/g, capture => {
+      return capture.replace(' ', ',')
+    })
+    capture
+    capture = `function ${capture}`
+    capture
+    
+    return capture
+  })
+
+  // replaces ':=' statements without left sides with 'return'
+  bang = bang.replace(/\n\s*:=/g, '\nreturn ')
+
+  // fixed variables
+  bang = bang.replace(/(^|\n)\s*[\[{]?\s*[A-Za-z\$]+?(\s*[A-Za-z\$]+?)*\s*[\]}]?\s*:?=/g, capture => {
+
+    capture = capture
+      .replace(/([^:]+):/g, 'let \$1')
+      .replace(/\n/g, '')
+
+    return `\n${capture}`
+  })
+
   bang = bang.replace(/\([A-Za-z\$]+?(\s[A-Za-z\$]+?)+\)/g, capture => {
     return capture.replace(/\s/g, ',')
   })
 
+  // fix all array literals
+  // REFACTOR: prone to error; redesign.
+  // REFACTOR: prone to error; redesign.
+  // REFACTOR: prone to error; redesign.
+  bang = bang.replace(/\[\s*(`.*?`|".*?"|'.*?'|[A-Za-z\$]+)(\s+(`.*?`|".*?"|'.*?'|[A-Za-z\$]+))*\s*\]/g, capture => {
+
+    capture
+    capture = capture.replace(/(\[|\s+)(`.*?`|".*?"|'.*?'|[A-Za-z\$]+)(\s+|\])/g, ',$2,')
+    capture
+    capture = capture.replace(']', '').replace('[', '')
+    capture
+    capture = capture.replace(/^,(.*?),?$/g, '[$1]')
+    capture
+
+    return capture
+  })
+
+  bang = bang.replace(/{\s*[A-Za-z\$]+(\s+[A-Za-z\$]+)*\s*}/g, capture => {
+    capture
+    capture = capture.replace(/([A-Za-z\$]+)\s+/g, '$1,')
+    capture
+    capture = capture.replace(',}', '}')
+    capture
+    return capture
+  })
+
+  bang = bang.replace(/{\s*([A-Za-z\$]+|'[^']*'|"[^"]*"|`[^`]*`):.+?\s*}/g, capture => {
+    // capture
+    capture = capture.replace(/:(.*?)\s+/g, ':$1,')
+    // capture
+    return capture
+  })
+  
   // fixes all string template literals
-  bang = bang.replace(/'.*?'|".*?"|`.*?`/g, capture => {
+  bang = bang.replace(/'.*?{.*?}.*?'|".*?{.*?}.*?"|`.*?{.*?}.*?`/g, capture => {
     // capture
     return capture.replace(/{(.+?)}/g, `\${\$1}`)
       .replace(/'/g, "`")
@@ -44,28 +93,22 @@ function compile(bang) {
       })
       .replace(/,\(/g, `} from '`)
       .replace(/\)/g, `'`)
-    return `import {${capture}`
+      .replace(/\n/g, '')
+    return `import {${capture}\n`
   })
 
-  // fixes function signatures
-  bang = bang.replace(/[A-Za-z\$]+\s?\([A-Za-z\$]+( [A-Za-z\$]+)*\)\s?{/g, capture => {
-    // capture
-    
-    return `function ${capture.replace(/\([A-Za-z\$]+( [A-Za-z\$]+)*\)/g, capture => {
-      return capture.replace(' ', ',')
-    })}`
-  })
+  bang = bang
+    .replace(/(^|\n)\s*log\s*\(/g, '\nconsole.log(')
+    .replace(/(^|\n)\s*warn\s*\(/g, '\nconsole.warn(')
+    .replace(/(^|\n)\s*error\s*\(/g, '\nconsole.error(')
 
-  // replaces '=' statements without left sides with 'return'
-  bang = bang.replace(/\n\s*=/g, '\nreturn ')
-  
   // removes all empty lines
   bang = bang.replace(/\n\s*\n/g, '\n')
 
   // trims all indentation
   // bang = bang.replace(/\n\s*/g, '\n')
-  
-  bang
+
+  return bang
 }
 
 const bangSource = `
@@ -75,8 +118,8 @@ const bangSource = `
 Min Max Abs (./Math)
 
 // Interprets all global assignments as 'let' under the hood.
-x = false
-y = true
+x := false
+y := true
 x = true
 
 // Assumes anything between an 'if' and its nearest '{' should be (wrapped).
@@ -85,12 +128,14 @@ if x {
   if y = true {
 
     // All 3-types of string literals are string templates.
-    xStr = 'x {x}'
-    yStr = "y {y}"
-    xAndY = \`x&y {x && y}\`
+    xStr := 'x {x}'
+    yStr := "y {y}"
+    xAndY := \`x&y {x && y}\`
     
     // Injects our variables into the template.
-    console.log('{xStr}, {yStr}, {xAndY}')
+    log('Howdy!')
+    warn('Partner.')
+    error('{xStr}, {yStr}, {xAndY}')
 
   }
 
@@ -98,17 +143,18 @@ if x {
 
 // A private function.
 addNums(fst snd) {
-  =fst + snd
+  := fst + snd
 }
 
-fst snd = ['one' 'two']
-counts = {one:1 two:2 three:3 'num four':4}
+dos := "two"
+[fst snd] := ['one' dos \`number three\`]
+counts := {one:1 two:2 three:3 'num four':4 "egg":x}
 
-two three = counts
-uno = counts[fst]
-quad = counts.'num four'
+{two three } := counts
+uno := counts[fst]
+quad := counts['num four']
 
 addNums(uno quad)
 `
 
-compile(bangSource)
+console.log(compile(bangSource))
